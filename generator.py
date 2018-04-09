0f84e982540463cb0ae6d2fbe2568f771f4391bf
Alice <alice.strt@gmail.com>
2018-04-09 22:52:58 +0200

    now creates simple slogan

--- generator.py
+++ generator.py
@@ -1,73 +1,95 @@
 #get text and split into tokens and parts of speech
 import nltk
 from nltk import pos_tag, word_tokenize, WhitespaceTokenizer
+import sys
+from sys import stdin
+import random
 
-def POS_tagger(list):
-    taggedwordlist = nltk.pos_tag(list)
+def tags(tag):
+    if tag in {"NNP","NNS","NN","NNPS"}:
+        POS_tag = 'noun'
+    elif tag in {'VB','VBD','VBG','VBN','VBP','VBZ'}:
+        POS_tag = 'verb'
+    elif tag in {'RB','RBR','RBS','WRB', 'RP'}:
+        POS_tag = 'adverb'
+    elif tag in {'PRP','PRP$'}:
+        POS_tag = 'pronoun'
+    elif tag in {'JJ','JJR','JJS'}:
+        POS_tag = 'adjective'
+    elif tag == 'IN':
+        POS_tag = 'preposition'
+    elif tag == 'WDT':
+        POS_tag = 'determiner'
+    elif tag in {'WP','WP$'}:
+        POS_tag = 'pronoun'
+    elif tag == 'UH':
+        POS_tag = 'interjection'
+    elif tag == 'POS':
+        POS_tag = 'possesive ending'
+    elif tag == 'SYM':
+        POS_tag = 'symbol'
+    elif tag == 'EX':
+        POS_tag = 'existential there'
+    elif tag == 'DT':
+        POS_tag = 'determiner'
+    elif tag == 'MD':
+        POS_tag = 'modal'
+    elif tag == 'LS':
+        POS_tag = 'list item marker'
+    elif tag == 'FW':
+        POS_tag = 'foreign word'
+    elif tag == 'CC':
+        POS_tag = 'coordinating conjunction '
+    elif tag == 'CD':
+        POS_tag = 'cardinal number'
+    elif tag == 'TO':
+        POS_tag = 'to'
+    elif tag == '.':
+        POS_tag = 'line ending'
+    elif tag == ',':
+        POS_tag = 'comma'
+    else:
+        POS_tag = tag
+    return POS_tag
 
-    for word, pos in nltk.pos_tag(list):
-        taggedwordlist = nltk.pos_tag(list)
+def POS_tagger(words):
+    taggedwordlist = nltk.pos_tag(words)
 
     taglist = [pos for word,pos in taggedwordlist]
     POS_tags = []
 
-    for tag in taglist:
-        if tag in {"NNP","NNS","NN","NNPS"}:
-            POS_tag = 'noun'
-        elif tag in {'VB','VBD','VBG','VBN','VBP','VBZ'}:
-            POS_tag = 'verb'
-        elif tag in {'RB','RBR','RBS','WRB', 'RP'}:
-            POS_tag = 'adverb'
-        elif tag in {'PRP','PRP$'}:
-            POS_tag = 'pronoun'
-        elif tag in {'JJ','JJR','JJS'}:
-            POS_tag = 'adjective'
-        elif tag == 'IN':
-            POS_tag = 'preposition'
-        elif tag == 'WDT':
-            POS_tag = 'determiner'
-        elif tag in {'WP','WP$'}:
-            POS_tag = 'pronoun'
-        elif tag == 'UH':
-            POS_tag = 'interjection'
-        elif tag == 'POS':
-            POS_tag = 'possesive ending'
-        elif tag == 'SYM':
-            POS_tag = 'symbol'
-        elif tag == 'EX':
-            POS_tag = 'existential there'
-        elif tag == 'DT':
-            POS_tag = 'determiner'
-        elif tag == 'MD':
-            POS_tag = 'modal'
-        elif tag == 'LS':
-            POS_tag = 'list item marker'
-        elif tag == 'FW':
-            POS_tag = 'foreign word'
-        elif tag == 'CC':
-            POS_tag = 'coordinating conjunction '
-        elif tag == 'CD':
-            POS_tag = 'cardinal number'
-        elif tag == 'TO':
-            POS_tag = 'to'
-        elif tag == '.':
-            POS_tag = 'line ending'
-        elif tag == ',':
-            POS_tag = 'comma'
+    for item in taggedwordlist:
+        postag = tags(item[1])
+        POS_tags.append([item[0], postag])
+
+    return POS_tags
+
+def givemeone(postag, allthewords):
+    filtered = []
+    for wordgroup in allthewords:
+        if wordgroup[1] == postag:
+            filtered.append(wordgroup[0])
+    return random.choice(filtered)
+
+def alltags(allthewords):
+    return [t[1] for t in allthewords]
+
+def doslogan():
+    transcript = open('slogans.txt').read()
+    words = WhitespaceTokenizer().tokenize(transcript)
+    tagged = POS_tagger(words)
+
+    tags = alltags(tagged)
+
+    newlist = []
+    structure1 = ['noun', 'not', 'noun']
+    for index, item in enumerate(structure1):
+        if item in tags:
+            one = givemeone(item, tagged)
+            newlist.append(one)
         else:
-            POS_tag = tag
+            newlist.append(item)
+    print(' '.join(newlist))
 
-        POS_tags.append(POS_tag)
-    return POS_tags;
-
-transcript = open('ounupo.txt', 'r')
-words = WhitespaceTokenizer().tokenize(transcript.read())
-words_and_tags = {index : {'word':word} for index , word in enumerate(words)}
-POS_tags = POS_tagger(words)
-i = 0
-for item, value in words_and_tags.items():
-    word = words_and_tags[item]['word']
-    pos_tag = POS_tags[i]
-    words_and_tags[item]['POS'] = pos_tag
-    i += 1
-print(words_and_tags)
+for _ in range(1, 10):
+    doslogan()
